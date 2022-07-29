d0550903b0838beb03c7b2c19c3c2c2e243fc2bd
@dlcanegro <garciaolivo89@gmail.com>
2022-07-29 10:52:23 -0300

    first2 commit

--- script.js
+++ script.js
@@ -9,12 +9,3 @@ container.style.width=`${w}px`
 container.style.height=`${h}px`
 
 
-function resizeContainer(){
-    var container=document.getElementById('container')
-
-    var w = window.innerWidth;
-    var h = window.innerHeight;
-
-    container.style.width=`${w}px`;
-    container.style.height=`${h}px`
-}
