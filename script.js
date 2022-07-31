ea6cdc77a769f3eb153a3ca50bcb55823f92467b
Fergolive <garciaolivo89@gmail.com>
2022-07-31 01:24:40 -0300

    responsive container

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
-
+/* 
 var container=document.getElementById('container')
 
 var w = window.innerWidth;
 var h = window.innerHeight;
 
 container.style.width=`${w}px`
-container.style.height=`${h}px`
+container.style.height=`${h}px` */
 
 
